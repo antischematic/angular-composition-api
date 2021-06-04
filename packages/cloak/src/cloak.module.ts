85985845db5cfac1aa329219417eb0aa7f7a517d
Michael Muscat <michael.muscat@outlook.com>
2021-06-04 22:56:30 +1000

    feat: add ng-cloak

---
+++ packages/cloak/src/cloak.module.ts
@@ -0,0 +1,6 @@
+import { NgModule } from '@angular/core';
+import { CloakComponent } from './cloak.component';
+
+
+
+
---
+++ packages/cloak/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of cloak
+ */
+
+export * from './lib/cloak.service';
+export * from './cloak.component';
+export * from './cloak.module';
