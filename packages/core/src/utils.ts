c27e6e7182b8bf0f0b61e0cccdd31df3693fe312
mmuscat <michael.muscat@outlook.com>
2021-11-16 09:59:34 +1000

    feat(core): add `pipe` feature

--- packages/core/src/utils.ts
+++ packages/core/src/utils.ts
@@ -1,4 +1,11 @@
-import { PartialObserver, Subscription } from "rxjs"
+import {
+   isObservable,
+   Observable,
+   PartialObserver,
+   Subscription,
+   UnaryFunction,
+   identity
+} from "rxjs"
 import {
    Emitter,
    ExpandValue,
@@ -6,6 +13,7 @@ import {
    UnsubscribeSignal,
    Value,
 } from "./interfaces"
+import { use } from "./common"
 
 export function isObject(value: unknown): value is {} {
    return typeof value === "object" && value !== null
@@ -65,7 +73,7 @@ export function observeNotification<T>(
 
 export function getPath(value: any, path: string[]): any {
    if (!path.length) return value
-   return path.reduceRight((val: any, key) => val[key], value)
+   return path.reduceRight((val: any, key) => val?.[key], value)
 }
 
 export function walk<T extends { [key: string]: any }>(
@@ -97,4 +105,35 @@ export function access<T>(value: Value<T>): T
 export function access(value: any) {
    if (isValue(value)) return value.value
    return walk(value, (current) => (isValue(current) ? current.value : current))
+}
+
+export function pipe(): typeof identity;
+export function pipe<T, A>(fn1: UnaryFunction<T, A>): UnaryFunction<T, A>;
+export function pipe<T, A, B>(fn1: UnaryFunction<T, A>, fn2: UnaryFunction<A, B>): UnaryFunction<T, B>;
+export function pipe<T, A, B, C>(fn1: UnaryFunction<T, A>, fn2: UnaryFunction<A, B>, fn3: UnaryFunction<B, C>): UnaryFunction<T, C>;
+export function pipe<T, A, B, C, D>(fn1: UnaryFunction<T, A>, fn2: UnaryFunction<A, B>, fn3: UnaryFunction<B, C>, fn4: UnaryFunction<C, D>): UnaryFunction<T, D>;
+export function pipe<T, A, B, C, D, E>(fn1: UnaryFunction<T, A>, fn2: UnaryFunction<A, B>, fn3: UnaryFunction<B, C>, fn4: UnaryFunction<C, D>, fn5: UnaryFunction<D, E>): UnaryFunction<T, E>;
+export function pipe<T, A, B, C, D, E, F>(fn1: UnaryFunction<T, A>, fn2: UnaryFunction<A, B>, fn3: UnaryFunction<B, C>, fn4: UnaryFunction<C, D>, fn5: UnaryFunction<D, E>, fn6: UnaryFunction<E, F>): UnaryFunction<T, F>;
+export function pipe<T, A, B, C, D, E, F, G>(fn1: UnaryFunction<T, A>, fn2: UnaryFunction<A, B>, fn3: UnaryFunction<B, C>, fn4: UnaryFunction<C, D>, fn5: UnaryFunction<D, E>, fn6: UnaryFunction<E, F>, fn7: UnaryFunction<F, G>): UnaryFunction<T, G>;
+export function pipe<T, A, B, C, D, E, F, G, H>(fn1: UnaryFunction<T, A>, fn2: UnaryFunction<A, B>, fn3: UnaryFunction<B, C>, fn4: UnaryFunction<C, D>, fn5: UnaryFunction<D, E>, fn6: UnaryFunction<E, F>, fn7: UnaryFunction<F, G>, fn8: UnaryFunction<G, H>): UnaryFunction<T, H>;
+export function pipe<T, A, B, C, D, E, F, G, H, I>(fn1: UnaryFunction<T, A>, fn2: UnaryFunction<A, B>, fn3: UnaryFunction<B, C>, fn4: UnaryFunction<C, D>, fn5: UnaryFunction<D, E>, fn6: UnaryFunction<E, F>, fn7: UnaryFunction<F, G>, fn8: UnaryFunction<G, H>, fn9: UnaryFunction<H, I>): UnaryFunction<T, I>;
+export function pipe<T, A, B, C, D, E, F, G, H, I>(fn1: UnaryFunction<T, A>, fn2: UnaryFunction<A, B>, fn3: UnaryFunction<B, C>, fn4: UnaryFunction<C, D>, fn5: UnaryFunction<D, E>, fn6: UnaryFunction<E, F>, fn7: UnaryFunction<F, G>, fn8: UnaryFunction<G, H>, fn9: UnaryFunction<H, I>, ...fns: UnaryFunction<any, any>[]): UnaryFunction<T, unknown>
+export function pipe<T, A>(source: Observable<T>, fn1: UnaryFunction<T, A>): Value<A | undefined>;
+export function pipe<T, A, B>(source: Observable<T>, fn1: UnaryFunction<T, A>, fn2: UnaryFunction<A, B>): Value<B | undefined>;
+export function pipe<T, A, B, C>(source: Observable<T>, fn1: UnaryFunction<T, A>, fn2: UnaryFunction<A, B>, fn3: UnaryFunction<B, C>): Value<C | undefined>;
+export function pipe<T, A, B, C, D>(source: Observable<T>, fn1: UnaryFunction<T, A>, fn2: UnaryFunction<A, B>, fn3: UnaryFunction<B, C>, fn4: UnaryFunction<C, D>): Value<D | undefined>;
+export function pipe<T, A, B, C, D, E>(source: Observable<T>, fn1: UnaryFunction<T, A>, fn2: UnaryFunction<A, B>, fn3: UnaryFunction<B, C>, fn4: UnaryFunction<C, D>, fn5: UnaryFunction<D, E>): Value<E | undefined>;
+export function pipe<T, A, B, C, D, E, F>(source: Observable<T>, fn1: UnaryFunction<T, A>, fn2: UnaryFunction<A, B>, fn3: UnaryFunction<B, C>, fn4: UnaryFunction<C, D>, fn5: UnaryFunction<D, E>, fn6: UnaryFunction<E, F>): Value<F | undefined>;
+export function pipe<T, A, B, C, D, E, F, G>(source: Observable<T>, fn1: UnaryFunction<T, A>, fn2: UnaryFunction<A, B>, fn3: UnaryFunction<B, C>, fn4: UnaryFunction<C, D>, fn5: UnaryFunction<D, E>, fn6: UnaryFunction<E, F>, fn7: UnaryFunction<F, G>): Value<G | undefined>;
+export function pipe<T, A, B, C, D, E, F, G, H>(source: Observable<T>, fn1: UnaryFunction<T, A>, fn2: UnaryFunction<A, B>, fn3: UnaryFunction<B, C>, fn4: UnaryFunction<C, D>, fn5: UnaryFunction<D, E>, fn6: UnaryFunction<E, F>, fn7: UnaryFunction<F, G>, fn8: UnaryFunction<G, H>): Value<H | undefined>;
+export function pipe<T, A, B, C, D, E, F, G, H, I>(source: Observable<T>, fn1: UnaryFunction<T, A>, fn2: UnaryFunction<A, B>, fn3: UnaryFunction<B, C>, fn4: UnaryFunction<C, D>, fn5: UnaryFunction<D, E>, fn6: UnaryFunction<E, F>, fn7: UnaryFunction<F, G>, fn8: UnaryFunction<G, H>, fn9: UnaryFunction<H, I>): Value<I | undefined>;
+export function pipe<T, A, B, C, D, E, F, G, H, I>(source: Observable<T>, fn1: UnaryFunction<T, A>, fn2: UnaryFunction<A, B>, fn3: UnaryFunction<B, C>, fn4: UnaryFunction<C, D>, fn5: UnaryFunction<D, E>, fn6: UnaryFunction<E, F>, fn7: UnaryFunction<F, G>, fn8: UnaryFunction<G, H>, fn9: UnaryFunction<H, I>, ...fns: UnaryFunction<any, any>[]): Value<unknown | undefined>;
+export function pipe(...args: any[]): unknown {
+   if (isObservable(args[0])) {
+      return use((<any>args[0]).pipe(...args.slice(1)))
+   } else {
+      return function (source: Observable<unknown>) {
+         return (<any>pipe)(source, ...args)
+      }
+   }
 }
