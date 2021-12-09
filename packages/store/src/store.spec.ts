b89d520f91739b0e3d4fe345f38236ac0d5a5367
mmuscat <michael.muscat@outlook.com>
2021-12-09 21:34:20 +1000

    build: 0.1307.0 release

--- packages/store/src/store.spec.ts
+++ packages/store/src/store.spec.ts
@@ -22,7 +22,6 @@ import { bufferCount, interval, map, merge, of, switchMap } from "rxjs"
 import { Store } from "./store"
 import { Effect } from "./effect"
 import { action } from "./utils"
-import { StoreLog } from "./plugins/store-log"
 import createSpy = jasmine.createSpy
 
 abstract class Service {}
@@ -231,7 +230,6 @@ describe("Store", () => {
       })
       const AppStore = new Store("app", {
          tokens: [query, command, effect],
-         plugins: [StoreLog],
       })
       addProvider(AppStore.Provider)
       subscribe(TestBed.inject(query), spy)
